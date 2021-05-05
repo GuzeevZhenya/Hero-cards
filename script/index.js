0bf58bd18e250396e0aece8568ef29ee15cb448b
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-05-06 00:28:25 +0300

    done

--- script/index.js
+++ script/index.js
@@ -1,44 +1,115 @@
+//Из того что криво работает:
+//1) поисковик не до конца понятно как сделать, он выдает только тогда
+//когда имя написано правильно, я попробовал сделать,чтобы удаляло тире
+//и писало новое слово с большой буквы, но тогда не работает поиск
+//ant-man, остальные более или менее
+//2) косяк в выводе male и female, я так понимаю оно конфликтует из за 
+//условия
+//3) 4 карточка(black widow) не подсвечивается темным, т.к у нее статус жив, но при этом
+// есть дата смерти...хммммм вопросик, как тогда сделать? Условием аля
+// если жива и при этом есть дата смерти, то она жива????
+//4) ну и думал сделать чтобы поисковик выводил при вводе букв, искал
+//имена которые начинаются ,Но пока не до конца придумал как сделать
+
+
 window.addEventListener('DOMContentLoaded', () => {
-    const card = document.querySelector('.card__inner');
     const app = document.querySelector('.card');
-    const db = './db/dbHeroes.json';
-
-
-    function heroCards() {
+    const cardInput = document.querySelector('.card__input');
+    const buttons = document.querySelectorAll('.card__category');
+    const buttonMain = document.querySelector('.buttons');
+    const searchButton = document.querySelector('.searching-button');
+    //Получаем данные из сервера
+    function heroCards(status) {
         const db = 'https://heroescards-da99b-default-rtdb.europe-west1.firebasedatabase.app/db.json';
         fetch(db)
             .then(resp => resp.json())
-            .then(data => heroCardsInfo(data))
+            .then(data => heroCardsInfo(data, status))
             .catch((e) => alert(e));
     }
-
-    function heroCardsInfo(dataInfo) {
+    //Выводим данные на страницу
+    function heroCardsInfo(dataInfo, status) {
         app.textContent = '';
-        console.log(dataInfo);
         for (let i = 0; i < dataInfo.length; i++) {
-            app.appendChild(createHeroCards(dataInfo[i]))
+            if (status === 'all') {
+                app.appendChild(createHeroCards(dataInfo[i]))
+            }
+            if (dataInfo[i].name === status) {
+                app.appendChild(createHeroCards(dataInfo[i]))
+            }
+            // if (dataInfo[i].gender === 'female') {
+            //     app.appendChild(createHeroCards(dataInfo[i]))
+            // }
+
+            // if (dataInfo[i].gender === 'male') {
+            //     app.appendChild(createHeroCards(dataInfo[i]))
+            // }
+
+            if (dataInfo[i].status === status) {
+                app.appendChild(createHeroCards(dataInfo[i]))
+            }
         }
     }
 
+
+    cardInput.addEventListener('input',()=>{
+        let searchValue = cardInput.value;
+    //    let newWord= searchValue.split(/\s+/).map(word => word[0].toUpperCase() + word.splice(1)).join(' ')
+    
+    //  newWord = searchValue.replace(/(^|\s)\S/g, function(a) {return a.toUpperCase()})
+    let newWord = searchValue.replace(/-|\s/g," ");
+    let result = newWord.replace(/(^|\s)\S/g, function(a) {return a.toUpperCase()})
+    // let result2 = result.trim().replace(/[^a-zA-Z0-9 -]/, "").replace(/\s/g, "-");
+         heroCards(result);
+    })
+
+    buttonMain.addEventListener('click', (e) => {
+        let target = e.target;
+
+        if (target.classList.contains('button-dead')) {
+            heroCards('deceased');
+        }
+        if (target.classList.contains('button-alive')) {
+            heroCards('alive');
+        }
+        if (target.classList.contains('button-all')) {
+            heroCards('all');
+        }
+        if (target.classList.contains('button-female')) {
+            heroCards('female');
+        }
+        if (target.classList.contains('button-male')) {
+            heroCards('male');
+        }
+    })
+
+
+    //Создаем карточки героев
     function createHeroCards(dataInfo) {
         const cardMarkup = `
-            <div class="card__face card__face--front">
+            <div class="card__face card__face--front ${dataInfo.status ==='deceased'?'dead-style':'alive-style'}">
                 <h2>${dataInfo.name}</h2>
             </div>
             <div class="card__face card__face--back">
                 <div class="card__content">
-                    <div class="card__header">
+                    <div class="card__header ${dataInfo.status ==='deceased'?'dead-style':'alive-style'}">
                         <img src="${dataInfo.photo}" alt="" class="pp" />
-                        <h2>${dataInfo.realName}</h2>
+                        <h2>${dataInfo.realName?dataInfo.realName:dataInfo.name}</h2>
                     </div>
-                    <div class="card__body">
-                        <h3>JavaScript Wizard</h3>
-                        <p>Lorem ipsum <strong>dolor</strong> sit amet, consectetur <strong>adipiscing</strong> elit.
-                            Sed id erat a magna lobortis dictum. Nunc est arcu, <strong>lacinia</strong> quis sapien
-                            placerat, <strong>laoreet</strong> tincidunt nulla.</p>
+                     
+                        <h3></h3>
+                         <ul class="card__body">
+                            <li><span>Дата рождения: ${dataInfo.birthDay?dataInfo.birthDay:'Неизвестно'}</li>
+                            <li></li>
+                            <li><span>Пол: ${dataInfo.gender}</li>
+                            <li><span>Раcа: ${dataInfo.species}</li>
+                            <li><span>Статус: ${dataInfo.status} ${dataInfo.deathDay?'' + dataInfo.deathDay:''}</li>
+                            <li></li>
+                            <li></li>
+                         </ul>
                     </div>
                 </div>
-    </div>`
+        </div>`
+
         const cardBlock = document.createElement('div');
         cardBlock.className = `card__inner ${dataInfo.status}`;
         cardBlock.innerHTML = cardMarkup;
@@ -48,32 +119,4 @@ window.addEventListener('DOMContentLoaded', () => {
         return cardBlock;
     }
 
-    heroCards();
-
-    function sort() {
-        const buttons = document.querySelectorAll('.card__category');
-        const cards = document.querySelectorAll('.card__inner');
-       
-
-        function filter(category, items) {
-            items.forEach(item => {
-                const isItemFiltered = !item.classList.contains(category);
-                const isShowAll = category.toLowerCase() === 'all';
-                if (isItemFiltered && !isShowAll) {
-                    item.classList.add('hide');
-                } else {
-                    item.classList.remove('hide');
-                }
-            })
-        }
-        buttons.forEach(button => {
-            button.addEventListener('click', (e) => {
-                console.log(cards);
-                const currentCategory = button.dataset.filter;
-                console.log(currentCategory)
-                filter(currentCategory, cards);
-            });
-        });
-    }
-    sort()
 });
